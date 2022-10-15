909af97157895810e036585cd98f0d6d8be51c43
sairi <sairi@debian>
2022-10-15 13:26:14 -0400

    autosubmit detail user dalam submition

--- base/models.py
+++ base/models.py
@@ -2,36 +2,38 @@ from contextlib import nullcontext
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+
 
 class User(AbstractUser):
     name = models.CharField(max_length=100, null=True)
     email = models.EmailField(unique=True)
     bio = models.TextField(null=True, blank=True)
-    
+
     hackathon_participant = models.BooleanField(default=True, null=True)
-    
-    
-    #avatar = 
-    USERNAME_FIELD: 'email'
-    REQUIRED_FIELDS: ['username']
-    
+
+    # avatar =
+    USERNAME_FIELD: "email"
+    REQUIRED_FIELDS: ["username"]
+
+
 class Event(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants =  models.ManyToManyField(User, blank=True, related_name='events')
+    participants = models.ManyToManyField(User, blank=True, related_name="events")
     date = models.DateTimeField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class Submission(models.Model):
     participant = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
     details = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return str(self.event) + '---' + str(self.participant)
-        
+        return str(self.event) + "---" + str(self.participant)
